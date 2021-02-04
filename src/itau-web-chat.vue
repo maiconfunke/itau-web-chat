8840d1422532b1f9c877f73dd83d3764717c86f3
Maicon Patrick Funke <maicon.funke@totvs.com.br>
2021-02-04 08:12:22 -0300

    init

---
+++ src/itau-web-chat.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+      <h1>Hello Word</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItauWebChat',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: red;
+  font-size: 40px;
+}
+</style>
